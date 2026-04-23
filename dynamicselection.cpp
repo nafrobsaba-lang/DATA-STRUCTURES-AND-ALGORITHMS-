7617aadc3a7c89b1b9f4a4860ab45968677b58e4
Nafrob Husen <nafrobsaba@gmail.com>
2026-04-23 12:10:41 -0700

    dynamic

---
+++ dynamicselection.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;8
+    cout << "enter the number of elements: ";
+    cin >> n;
+    int arr[n]; 
+    cout << "enter " << n << " elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+
+    cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
